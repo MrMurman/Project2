faeba3a8c4ad109c35ee1198ab95afac277e5a1c
MrMurman <storm2150@yandex.ru>
2021-06-22 22:36:53 +0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Андрей Бородкин on 22.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
